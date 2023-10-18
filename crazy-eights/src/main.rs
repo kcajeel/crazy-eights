2813150fa178a06a715ae7bf9685b03ce7328c17
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-10-18 12:04:58 -0400

    there has to be a better way to do this

--- crazy-eights/src/main.rs
+++ crazy-eights/src/main.rs
@@ -1,24 +1,41 @@
-
-enum Card {
-    Suit,
-    Number,
+struct Card {
+    suit: String,
+    number: i32,
 }
 
+impl Card {}
+
 fn main() {
-    let mut deck: Vec<Card> = vec![];
+    let deck: Vec<Card> = vec![];
     initialize_deck(deck);
     println!("Crazy Eights Game!");
 }
 
-fn initialize_deck(deck: Vec<Card>) -> Vec<Card> {
+fn initialize_deck(mut deck: Vec<Card>) -> Vec<Card> {
     for i in 1..52 {
-        // card number = i%13
-            // if i%13 == 0, card number is 13 (King)
-        // suit = match floor(52/i)
-            // 4 -> hearts
-            // 3 -> diamonds
-            // 2 -> clubs
-            // 1 -> spades
+        deck.push(Card {
+            number: if i % 13 != 0 { i % 13 } else { 13 },
+            suit: if i / 13 < 1 {
+                "Hearts".to_string()
+            } else if i / 13 > 1 && i / 13 < 2 {
+                "Diamonds".to_string()
+            } else if i / 13 > 2 && i / 13 < 3 {
+                "Clubs".to_string()
+            } else {
+                "Spades".to_string()
+            },
+        })
     }
+    /*
+    suit
+    i/13:
+    < 1: hearts
+    > 1 && < 2: diamonds
+    > 2 && < 3: clubs
+    > 3 && < 4: spades
+    */
+
+    // card number = i%13
+    // if i%13 == 0, card number is 13 (King)
     deck
 }
