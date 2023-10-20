a97c58a7a066f3c4b257f00ebf57aea37dbd3c4f
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-10-19 22:35:52 -0400

    deck creation and printing works

--- crazy-eights/src/main.rs
+++ crazy-eights/src/main.rs
@@ -3,25 +3,29 @@ struct Card {
     number: i32,
 }
 
-impl Card {}
-
 fn main() {
-    let deck: Vec<Card> = vec![];
-    initialize_deck(deck);
-    println!("Crazy Eights Game!");
+    let mut deck: Vec<Card> = vec![];
+    initialize_deck(&mut deck);
+    println!("Listing cards: \n");
+    print_deck(deck);
 }
 
-fn initialize_deck(mut deck: Vec<Card>) -> Vec<Card> {
-    for i in 1..53 {
+fn initialize_deck(deck: &mut Vec<Card>) {
+    for i in 0..52 {
+        print!("i/13: {}, i: {}\n", i / 13, i);
         deck.push(Card {
             number: if i % 13 != 0 { i % 13 } else { 13 },
             suit: if i / 13 < 1 {
+                // 0
                 "Hearts".to_string()
-            } else if i / 13 > 1 && i / 13 < 2 {
+            } else if i / 13 > 0 && i / 13 < 2 {
+                // 1
                 "Diamonds".to_string()
-            } else if i / 13 > 2 && i / 13 < 3 {
+            } else if i / 13 > 1 && i / 13 < 3 {
+                // 2
                 "Clubs".to_string()
             } else {
+                // 3
                 "Spades".to_string()
             },
         })
@@ -37,5 +41,10 @@ fn initialize_deck(mut deck: Vec<Card>) -> Vec<Card> {
 
     // card number = i%13
     // if i%13 == 0, card number is 13 (King)
-    deck
+}
+
+fn print_deck(deck: Vec<Card>) {
+    for i in deck.iter() {
+        println!("Suit: {}, Number: {}", i.suit, i.number);
+    }
 }
