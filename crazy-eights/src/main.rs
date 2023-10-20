f665fb469e7659a90bf65095234d8429b4082f12
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-10-19 22:49:31 -0400

    added some comments

--- crazy-eights/src/main.rs
+++ crazy-eights/src/main.rs
@@ -14,7 +14,9 @@ fn initialize_deck(deck: &mut Vec<Card>) {
     for i in 0..52 {
         print!("i/13: {}, i: {}\n", i / 13, i);
         deck.push(Card {
+            // i%13 gives card number, if 0 then number = 13 (king)
             number: if i % 13 != 0 { i % 13 } else { 13 },
+            // i/13 gives card suit (52/13=4)
             suit: match i / 13 {
                 0 => "Hearts".to_string(),
                 1 => "Diamonds".to_string(),
