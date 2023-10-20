930a712fdd6df87753a450c46fffccc628584bb0
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-10-19 22:47:24 -0400

    changed initialize_deck to use match

--- crazy-eights/src/main.rs
+++ crazy-eights/src/main.rs
@@ -15,32 +15,15 @@ fn initialize_deck(deck: &mut Vec<Card>) {
         print!("i/13: {}, i: {}\n", i / 13, i);
         deck.push(Card {
             number: if i % 13 != 0 { i % 13 } else { 13 },
-            suit: if i / 13 < 1 {
-                // 0
-                "Hearts".to_string()
-            } else if i / 13 > 0 && i / 13 < 2 {
-                // 1
-                "Diamonds".to_string()
-            } else if i / 13 > 1 && i / 13 < 3 {
-                // 2
-                "Clubs".to_string()
-            } else {
-                // 3
-                "Spades".to_string()
+            suit: match i / 13 {
+                0 => "Hearts".to_string(),
+                1 => "Diamonds".to_string(),
+                2 => "Clubs".to_string(),
+                3 => "Spades".to_string(),
+                _ => "Joker???".to_string(),
             },
         })
     }
-    /*
-    suit
-    i/13:
-    < 1: hearts
-    > 1 && < 2: diamonds
-    > 2 && < 3: clubs
-    > 3 && < 4: spades
-    */
-
-    // card number = i%13
-    // if i%13 == 0, card number is 13 (King)
 }
 
 fn print_deck(deck: Vec<Card>) {
