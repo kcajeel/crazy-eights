daf5e3771f0f4f85abe5c68f1f5f1779ff8f91e9
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-12-29 21:57:35 -0500

    dead code cleanup

--- crazy-eights/src/deck.rs
+++ crazy-eights/src/deck.rs
@@ -19,45 +19,12 @@ enum Value {
     King,
 }
 
-impl Value {
-    pub fn from_int(value: i32) -> Option<Self> {
-        match value {
-            1 => Some(Value::Ace),
-            2 => Some(Value::Two),
-            3 => Some(Value::Three),
-            4 => Some(Value::Four),
-            5 => Some(Value::Five),
-            6 => Some(Value::Six),
-            7 => Some(Value::Seven),
-            8 => Some(Value::Eight),
-            9 => Some(Value::Nine),
-            10 => Some(Value::Ten),
-            11 => Some(Value::Jack),
-            12 => Some(Value::Queen),
-            13 => Some(Value::King),
-            _ => None,
-        }
-    }
-}
-
 #[derive(Clone, Debug, EnumIter)]
 enum Suit {
     Hearts,
     Diamonds,
     Spades,
     Clubs,
-}
-
-impl Suit {
-    pub fn from_str(str: &str) -> Option<Suit> {
-        match str {
-            "Hearts" => Some(Suit::Hearts),
-            "Diamonds" => Some(Suit::Diamonds),
-            "Spades" => Some(Suit::Spades),
-            "Clubs" => Some(Suit::Clubs),
-            _ => None
-        }
-    }
 }
 
 #[derive(Clone)]
@@ -68,7 +35,7 @@ pub struct Card {
 
 impl Card {
     pub fn new_deck() -> Vec<Self> {
-        let mut deck:Vec<Card> = vec![];
+        let mut deck: Vec<Card> = vec![];
         for suit in Suit::iter() {
             for value in Value::iter() {
                 deck.push(Card {
