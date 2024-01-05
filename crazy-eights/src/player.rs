bd5e2fd1e6086f4caafdc7a79a4fe7e3bb88c318
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-01-04 22:26:56 -0500

    player now derives PartialEq

--- crazy-eights/src/player.rs
+++ crazy-eights/src/player.rs
@@ -1,7 +1,6 @@
-use crate::
-    deck::{Card, Suit, Value}
-;
+use crate::deck::{Card, Suit, Value};
 
+#[derive(PartialEq)]
 pub struct Player {
     pub name: String,
     pub hand: Vec<Card>,
@@ -33,14 +32,19 @@ impl Player {
         //TODO: implement user input
     }
 
-    fn play_card(hand: &mut Vec<Card>, discard_pile: &mut Vec<Card>, card: &Card, suit_in_play: &mut Suit) {
+    fn play_card(
+        hand: &mut Vec<Card>,
+        discard_pile: &mut Vec<Card>,
+        card: &Card,
+        suit_in_play: &mut Suit,
+    ) {
         if hand.contains(card) {
             discard_pile.push(*card); //add card to discard pile
             hand.retain(|&x| x != *card); //delete card from hand
-            
-            let mut new_suit_in_play = card.suit;   //get new suit in play
+
+            let mut new_suit_in_play = card.suit; //get new suit in play
             if card.value == Value::Eight {
-                new_suit_in_play = Self::prompt_user_for_suit();    //change the suit if crazy eight
+                new_suit_in_play = Self::prompt_user_for_suit(); //change the suit if crazy eight
             }
             Self::change_suit_in_play(&suit_in_play, &new_suit_in_play);
         }
@@ -48,7 +52,7 @@ impl Player {
 
     fn prompt_user_for_suit() -> Suit {
         println!("You have played a Crazy Eight. Which suit would you like to enforce?");
-        
+
         //TODO: implement user input
     }
 
@@ -56,14 +60,17 @@ impl Player {
         old_suit = new_suit;
     }
 
-    pub fn take_turn(hand: &mut Vec<Card>, deck: &mut Vec<Card>, discard_pile: &mut Vec<Card>, suit_in_play: &mut Suit) {
+    pub fn take_turn(
+        hand: &mut Vec<Card>,
+        deck: &mut Vec<Card>,
+        discard_pile: &mut Vec<Card>,
+        suit_in_play: &mut Suit,
+    ) {
         if let Some(top_card) = discard_pile.pop() {
             let playable_cards = Self::get_playable_cards(hand, &top_card);
-            
+
             while playable_cards.is_empty() {
-                if deck.is_empty() {
-                    
-                }
+                if deck.is_empty() {}
                 Self::draw_card(hand, deck);
             }
 
