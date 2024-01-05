772d1b7da6f4b61d72fa000834b2f5b8adf30697
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-01-04 22:20:52 -0500

    finished initial Game module implementation

--- crazy-eights/src/game.rs
+++ crazy-eights/src/game.rs
@@ -1,78 +1,121 @@
-use crate::
-    deck::{Card, Suit, Value}
-;
+use crate::{
+    deck::{Card, Suit},
+    player::Player,
+};
 
-pub struct Player {
-    pub name: String,
-    pub hand: Vec<Card>,
+#[derive(PartialEq)]
+pub enum Game {
+    Running {
+        players: Vec<Player>,
+        deck: Vec<Card>,
+        discard_pile: Vec<Card>,
+        suit_in_play: Suit,
+    },
+    Over,
 }
 
-impl Player {
-    pub fn new(name: String, hand: Vec<Card>) -> Self {
-        Self { name, hand }
-    }
-
-    pub fn draw_card(hand: &mut Vec<Card>, deck: &mut Vec<Card>) {
-        match deck.pop() {
-            Some(card) => hand.push(card),
-            None => panic!(), //TODO: implement DeckEmptyError
-        }
-    }
-
-    fn get_playable_cards(hand: &Vec<Card>, top_card: &Card) -> Vec<Card> {
-        let mut playable_cards = vec![];
-        for card in hand {
-            if Card::is_similar(card, top_card) {
-                playable_cards.push(*card);
+impl Game {
+    fn new(number_of_players: i32, mut deck: Vec<Card>) -> Self {
+        if is_number_of_players_valid(number_of_players) {
+            Game::Running {
+                players: Self::initialize_players(
+                    number_of_players,
+                    Self::get_player_names(number_of_players),
+                ),
+                deck,
+                discard_pile: vec![],
+                suit_in_play: Suit::Clubs,
             }
-        }
-        playable_cards
-    }
-
-    fn prompt_user_for_card(cards: Vec<Card>) -> Card {
-        //TODO: implement user input
-    }
-
-    fn play_card(hand: &mut Vec<Card>, discard_pile: &mut Vec<Card>, card: &Card, suit_in_play: &mut Suit) {
-        if hand.contains(card) {
-            discard_pile.push(*card); //add card to discard pile
-            hand.retain(|&x| x != *card); //delete card from hand
-            
-            let mut new_suit_in_play = card.suit;   //get new suit in play
-            if card.value == Value::Eight {
-                new_suit_in_play = Self::prompt_user_for_suit();    //change the suit if crazy eight
-            }
-            Self::change_suit_in_play(&suit_in_play, &new_suit_in_play);
+        } else {
+            Game::Over
         }
     }
 
-    fn prompt_user_for_suit() -> Suit {
-        println!("You have played a Crazy Eight. Which suit would you like to enforce?");
-        
-        //TODO: implement user input
+    fn get_player_names(number_of_players: i32) -> Vec<String> {
+        //TODO user input ;)
+        vec!["example name".to_string()]
     }
 
-    fn change_suit_in_play(mut old_suit: &Suit, new_suit: &Suit) {
-        old_suit = new_suit;
+    fn initialize_players(number_of_players: i32, mut names: Vec<String>) -> Vec<Player> {
+        let mut players = Vec::with_capacity(number_of_players as usize);
+
+        for i in 0..number_of_players {
+            players.push(Player::new(
+                match names.pop() {
+                    Some(name) => name,
+                    None => "".to_string(),
+                },
+                vec![],
+            ));
+        }
+        players
     }
 
-    pub fn take_turn(hand: &mut Vec<Card>, deck: &mut Vec<Card>, discard_pile: &mut Vec<Card>, suit_in_play: &mut Suit) {
-        if let Some(top_card) = discard_pile.pop() {
-            let playable_cards = Self::get_playable_cards(hand, &top_card);
-            
-            while playable_cards.is_empty() {
-                if deck.is_empty() {
-                    
+    fn deal_cards(players: &mut Vec<Player>, deck: &mut Vec<Card>) {
+        let cards_per_player = if players.len() == 2 { 7 } else { 5 };
+
+        for i in 0..cards_per_player {
+            for mut player in players {
+                if let Some(card) = deck.pop() {
+                    player.hand.push(card);
                 }
-                Self::draw_card(hand, deck);
             }
-
-            Self::play_card(
-                hand,
-                discard_pile,
-                &Self::prompt_user_for_card(playable_cards),
-                suit_in_play,
-            );
         }
     }
+
+    pub fn initialize(&mut self) {
+        if let Game::Running {
+            players: players,
+            deck: deck,
+            discard_pile: discard_pile,
+            suit_in_play: mut suit_in_play,
+        } = self
+        {
+            Self::deal_cards(players, deck);
+            if let Ok(card) = initialize_discard_pile(deck, discard_pile) {
+                suit_in_play = card.suit;
+            }
+        }
+    }
+
+    pub fn play(&mut self) {
+        if let Game::Running {
+            players: players,
+            deck: deck,
+            discard_pile: discard_pile,
+            suit_in_play: suit_in_play,
+        } = self
+        {
+            for player in players {
+                while !player.hand.is_empty() {
+                    Player::take_turn(&mut player.hand, deck, discard_pile, suit_in_play);
+                }
+            }
+        }
+        self.end_game();
+    }
+
+    pub fn end_game(&mut self) {
+        let over = &mut Game::Over;
+        self = over;
+    }
+}
+
+fn initialize_discard_pile(
+    deck: &mut Vec<Card>,
+    discard_pile: &mut Vec<Card>,
+) -> Result<Card, String> {
+    if let Some(card) = deck.pop() {
+        discard_pile.push(card);
+        return Ok(card);
+    } else {
+        return Err("deck empty".to_string());
+    }
+}
+
+fn is_number_of_players_valid(number_of_players: i32) -> bool {
+    if number_of_players >= 2 && number_of_players <= 10 {
+        return true;
+    }
+    false
 }
