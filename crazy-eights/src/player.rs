beba5ced9d93569c74a9fc58df7d03cb8c78c9a8
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-01-08 21:00:56 -0500

    added print i/o, fixed logic errors

--- crazy-eights/src/player.rs
+++ crazy-eights/src/player.rs
@@ -2,7 +2,7 @@ use std::io;
 
 use crate::deck::{self, Card, Suit, Value};
 
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Player {
     pub name: String,
     pub hand: Vec<Card>,
@@ -14,14 +14,28 @@ impl Player {
     }
 
     pub fn draw_card(hand: &mut Vec<Card>, deck: &mut Vec<Card>) {
-        hand.push(deck.pop().expect("Error: deck empty"));
+        println!("Drawing from the deck. ");
+        if let Some(card) = deck.pop() {
+            hand.push(card);
+            println!("You drew the ");
+            card.print();
+        } else {
+            panic!("Error: deck empty");
+        }
     }
 
-    fn get_playable_cards(hand: &Vec<Card>, top_card: &Card) -> Vec<Card> {
+    fn get_playable_cards(hand: &Vec<Card>, top_card: &Card, suit_in_play: &mut Suit) -> Vec<Card> {
         let mut playable_cards = vec![];
+
         for card in hand {
-            if Card::is_similar(card, top_card) {
-                playable_cards.push(*card);
+            if top_card.suit != *suit_in_play {
+                if card.suit == *suit_in_play {
+                    playable_cards.push(*card);
+                }
+            } else {
+                if Card::is_similar(card, top_card) {
+                    playable_cards.push(*card);
+                }
             }
         }
         playable_cards
@@ -36,7 +50,7 @@ impl Player {
         io::stdin()
             .read_line(&mut user_input)
             .expect("failed to read line");
-        print!("You played \"{}\".\n", user_input);
+        print!("You played \"{}\".\n", user_input.trim());
 
         let input_vec: Vec<&str> = user_input.split(' ').collect();
 
@@ -45,13 +59,19 @@ impl Player {
         }
         // unwrap used below because input_vec.len > 3 so .get(0..2) should return Some
         Card {
-            value: match Value::try_from(*input_vec.get(0).unwrap()) {
+            value: match Value::try_from(input_vec.get(0).unwrap().trim()) {
                 Ok(val) => val,
-                Err(_e) => cards.get(0).unwrap().value,
+                Err(_e) => {
+                    println!("error parsing value \"{}\"", input_vec.get(0).unwrap());
+                    cards.get(0).unwrap().value
+                }
             },
-            suit: match Suit::try_from(*input_vec.get(2).unwrap()) {
+            suit: match Suit::try_from(input_vec.get(2).unwrap().trim()) {
                 Ok(suit) => suit,
-                Err(_e) => cards.get(0).unwrap().suit,
+                Err(_e) => {
+                    println!("error parsing suit \"{}\"", input_vec.get(2).unwrap());
+                    cards.get(0).unwrap().suit
+                }
             },
         }
     }
@@ -62,7 +82,7 @@ impl Player {
         io::stdin()
             .read_line(&mut user_input)
             .expect("could not read input");
-        println!("You selected: {}", user_input);
+        println!("You selected: {}", user_input.trim());
 
         match Suit::try_from(user_input.trim()) {
             Ok(suit) => suit,
@@ -73,8 +93,9 @@ impl Player {
         }
     }
 
-    fn change_suit_in_play<'a>(mut _old_suit: &'a Suit, new_suit: &'a Suit) {
-        _old_suit = new_suit;
+    fn change_suit_in_play(_old_suit: &mut Suit, new_suit: Suit) {
+        *_old_suit = new_suit;
+        println!("new suit: {}", _old_suit);
     }
 
     fn play_card(
@@ -91,7 +112,13 @@ impl Player {
             if card.value == Value::Eight {
                 new_suit_in_play = Self::prompt_user_for_suit(); //change the suit if crazy eight
             }
-            Self::change_suit_in_play(&suit_in_play, &new_suit_in_play);
+            println!(
+                "suit in play changed from {} to {}",
+                suit_in_play, new_suit_in_play
+            );
+            Self::change_suit_in_play(suit_in_play, new_suit_in_play);
+        } else {
+            println!("{} of {} not found in your hand. ", card.value, card.suit);
         }
     }
 
@@ -101,22 +128,31 @@ impl Player {
         discard_pile: &mut Vec<Card>,
         suit_in_play: &mut Suit,
     ) {
-        if let Some(top_card) = discard_pile.pop() {
-            let playable_cards = Self::get_playable_cards(hand, &top_card);
+        let binding = discard_pile.clone();
+        let top_card = binding
+            .get(discard_pile.len() - 1)
+            .expect("discard pile empty");
+        let mut playable_cards = Self::get_playable_cards(hand, &top_card.clone(), suit_in_play);
 
-            while playable_cards.is_empty() {
-                if deck.is_empty() {
-                    deck::shuffle_discard_pile(deck, discard_pile);
-                }
-                Self::draw_card(hand, deck);
+        print!(
+            "You have {} cards in your hand. Card to match: ",
+            hand.len()
+        );
+        top_card.print();
+        while playable_cards.is_empty() {
+            print!("No playable cards. ");
+            if deck.is_empty() {
+                println!("The deck is empty, shuffling the discard pile into the deck.");
+                deck::shuffle_discard_pile(deck, discard_pile);
             }
-
-            Self::play_card(
-                hand,
-                discard_pile,
-                &Self::prompt_user_for_card(playable_cards),
-                suit_in_play,
-            );
+            Self::draw_card(hand, deck);
+            playable_cards = Self::get_playable_cards(hand, top_card, suit_in_play);
         }
+        Self::play_card(
+            hand,
+            discard_pile,
+            &Self::prompt_user_for_card(playable_cards),
+            suit_in_play,
+        );
     }
 }
